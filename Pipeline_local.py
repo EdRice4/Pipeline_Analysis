227d1605188e17450d5055e0ca900082f1e0f9b4
EdRice4 <edwinricethe4th@gmail.com>
2016-03-22 22:21:39 -0400

    Changed default of no_proc argument to 1.

--- Pipeline_local.py
+++ Pipeline_local.py
@@ -89,8 +89,8 @@ class jModelTest(object):
         # You may also have to change the manner in which jModelTest is
         # called, depending on your system.
         jModelTest = (
-                "java -jar {0} -d {1} -t fixed -s 11 -i -g 4 -f "
-                "-v -a -BIC -AIC -AICc -DT -tr {2}"
+                'java -jar {0} -d {1} -t fixed -s 11 -i -g 4 -f '
+                '-v -a -BIC -AIC -AICc -DT -tr {2}'
                 ).format(args.jMT, self._nexus_file, args.no_proc)
         # Spawn child process and run
         # TODO(Edwin):
@@ -1171,7 +1171,7 @@ class NexusFile(bGMYC):
                         'script has knowledge of the environment and can '
                         'take full advantage of it.'
                         ),
-                default=0
+                default=1
                 )
     # }}}
 
